1a00b5decfe228761a420cf86eb47a24ed1d5daa
Lakshmi C D <lakshmi.cd1111@gmail.com>
2026-08-27 17:14:32 +0530

    Fix controller - replace test stub with actual dashboard logic

--- controller/WorkloadEstimationmodel.controller.js
+++ controller/WorkloadEstimationmodel.controller.js
@@ -1,16 +1,111 @@
-/*global QUnit*/
-
 sap.ui.define([
-	"workloadestimationmodel/controller/WorkloadEstimationmodel.controller"
-], function (Controller) {
-	"use strict";
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+], (Controller, JSONModel) => {
+    "use strict";
 
-	QUnit.module("WorkloadEstimationmodel Controller");
+    return Controller.extend("workloadestimationmodel.controller.WorkloadEstimationmodel", {
+        onInit() {
+            const oRolloutModel = new JSONModel({
+                Stations: [
+                    { Name: "", Switches: null, Size: "", TotalDays: "", TotalHours: "", EuroPerHour: 0, EstimatedCost: 0 }
+                ],
+                totalHours: 0,
+                totalCost: 0
+            });
+            this.getView().setModel(oRolloutModel, "rollout");
+        },
 
-	QUnit.test("I should test the WorkloadEstimationmodel controller", function (assert) {
-		var oAppController = new Controller();
-		oAppController.onInit();
-		assert.ok(oAppController);
-	});
+        _classifySize(iSwitches) {
+            if (iSwitches <= 4) {
+                return "XS";
+            } else if (iSwitches < 6) {
+                return "S";
+            } else if (iSwitches < 10) {
+                return "M";
+            } else if (iSwitches < 20) {
+                return "L";
+            }
+            return "XL";
+        },
 
+        onSwitchesChange(oEvent) {
+            const oInput = oEvent.getSource();
+            const oContext = oInput.getBindingContext("rollout");
+            const sPath = oContext.getPath();
+            const oRolloutModel = this.getView().getModel("rollout");
+
+            const iSwitches = parseFloat(oInput.getValue());
+
+            if (isNaN(iSwitches) || iSwitches < 0) {
+                oRolloutModel.setProperty(sPath + "/Size", "");
+                oRolloutModel.setProperty(sPath + "/TotalDays", "");
+                oRolloutModel.setProperty(sPath + "/TotalHours", "");
+                oRolloutModel.setProperty(sPath + "/EstimatedCost", 0);
+                this._updateRolloutTotals();
+                return;
+            }
+
+            const sSize = this._classifySize(iSwitches);
+            const aSizes = this.getView().getModel("data").getProperty("/Sizes");
+            const oMatch = aSizes.find((oRow) => oRow.SizeCategory === sSize);
+
+            oRolloutModel.setProperty(sPath + "/Size", sSize);
+            oRolloutModel.setProperty(sPath + "/TotalDays", oMatch.TotalDays);
+            oRolloutModel.setProperty(sPath + "/TotalHours", oMatch.TotalHours);
+
+            const fRate = parseFloat(oRolloutModel.getProperty(sPath + "/EuroPerHour")) || 0;
+            oRolloutModel.setProperty(sPath + "/EstimatedCost", oMatch.TotalHours * fRate);
+
+            this._updateRolloutTotals();
+        },
+
+        onRateChange(oEvent) {
+            const oInput = oEvent.getSource();
+            const oContext = oInput.getBindingContext("rollout");
+            const sPath = oContext.getPath();
+            const oRolloutModel = this.getView().getModel("rollout");
+
+            const fRate = parseFloat(oInput.getValue()) || 0;
+            const fHours = parseFloat(oRolloutModel.getProperty(sPath + "/TotalHours")) || 0;
+
+            oRolloutModel.setProperty(sPath + "/EstimatedCost", fHours * fRate);
+            this._updateRolloutTotals();
+        },
+
+        onAddStation() {
+            const oRolloutModel = this.getView().getModel("rollout");
+            const aStations = oRolloutModel.getProperty("/Stations");
+            aStations.push({ Name: "", Switches: null, Size: "", TotalDays: "", TotalHours: "", EuroPerHour: 0, EstimatedCost: 0 });
+            oRolloutModel.refresh();
+        },
+
+        onRemoveStation(oEvent) {
+            const oButton = oEvent.getSource();
+            const oContext = oButton.getBindingContext("rollout");
+            const sPath = oContext.getPath();
+            const iIndex = parseInt(sPath.split("/").pop(), 10);
+
+            const oRolloutModel = this.getView().getModel("rollout");
+            const aStations = oRolloutModel.getProperty("/Stations");
+            aStations.splice(iIndex, 1);
+            oRolloutModel.refresh();
+            this._updateRolloutTotals();
+        },
+
+        _updateRolloutTotals() {
+            const oRolloutModel = this.getView().getModel("rollout");
+            const aStations = oRolloutModel.getProperty("/Stations");
+
+            let fTotalHours = 0;
+            let fTotalCost = 0;
+            aStations.forEach((oStation) => {
+                fTotalHours += parseFloat(oStation.TotalHours) || 0;
+                fTotalCost += parseFloat(oStation.EstimatedCost) || 0;
+            });
+
+            oRolloutModel.setProperty("/totalHours", fTotalHours);
+            oRolloutModel.setProperty("/totalCost", fTotalCost);
+        }
+    });
 });
